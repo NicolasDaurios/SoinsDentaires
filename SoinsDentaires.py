1073f6f4f6d91ad8c6a972c8d5ab49f99b244d50
Nico Dau <131247652+NicolasDaurios@users.noreply.github.com>
2023-11-03 10:28:45 +0100

    SoinsDentaires.py

--- SoinsDentaires.py
+++ SoinsDentaires.py
@@ -7,8 +7,7 @@ Created on Thu Oct 19 17:58:04 2023
 
 import pandas as pd 
 import numpy as np 
-import streamlit as st 
-import seaborn as sns 
+import streamlit as st  
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
